1e16f01095d7b7db7a9bbd0d7feac0d8a2bac604
Sasha Prodan <prodan.sasha@gmail.com>
2018-09-01 19:16:45 -0500

    display raw data from file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import Data from './data';
 
 class App extends Component {
   render() {
+    const rows = Data.routes.map(route => {
+      return (
+        <tr>
+          <td>{route.airline}</td>
+          <td>{route.src}</td>
+          <td>{route.dest}</td>
+        </tr>
+      )
+    });
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <p>
-            Welcome to the app!
-          </p>
+          <img src="./equirectangular_world.jpg" alt="Airline routes" />
+          <table className="routes-table">
+            <thead>
+              <tr>
+                <th>Airline</th>
+                <th>Source Airport</th>
+                <th>Destination Airport</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rows}
+            </tbody>
+          </table>
         </section>
       </div>
     );
